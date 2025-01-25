f7b56dc33209aeba73ed4e3cbd5b00b1a7aee9b0
plo0py87 <ploopy0408@gmail.com>
2025-01-25 14:07:03 +0800

    add cart list item page

--- lib/cart_list_item.dart
+++ lib/cart_list_item.dart
@@ -1,11 +1,25 @@
 import 'package:f1/product_list_item.dart';
 
 class CartListItem {
-  final ProductListItem product;
-  final int quantity;
+  final Map<int, int> items;
+
+  void addToCart(ProductListItem product) {
+    if (!items.containsKey(product.id)) {
+      items[product.id] = 1;
+    } else {
+      items[product.id] = items[product.id]! + 1;
+    }
+  }
+
+  void quantityAdjust(ProductListItem product, int quantity) {
+    if (quantity == 0) {
+      items.remove(product.id);
+    } else {
+      items[product.id] = quantity;
+    }
+  }
 
   CartListItem({
-    required this.product,
-    required this.quantity,
+    required this.items,
   });
 }
--- lib/cart_list_item_page.dart
+++ lib/cart_list_item_page.dart
@@ -3,26 +3,73 @@ import 'product_list_item.dart';
 
 class CartListItemPage extends StatelessWidget {
   final Map<int, int> cartItems;
-  final List<Pro> products;
+  final List<ProductListItem> products;
+  final Function(ProductListItem,int) quantityAdjust;
+
+  const CartListItemPage({Key? key, required this.cartItems, required this.products,required this.quantityAdjust}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cart Items'),
+        title: const Text('Cart'),
       ),
-      body: ListView.builder(
+      body: ListView.separated(
         itemCount: cartItems.length,
         itemBuilder: (context, index) {
+          final productId = cartItems.keys.elementAt(index);
+          final product = products.firstWhere((product) => product.id == productId);
+          final quantity = cartItems[productId]!;
           return ListTile(
-            title: Text(cartItems[index]),
-            trailing: IconButton(
-              icon: Icon(Icons.remove_circle_outline),
-              onPressed: () {
-                // Handle item removal
-              },
+            subtitle: Text('Total: \$${(product.price * quantity).toStringAsFixed(2)}'),
+            leading: CircleAvatar(
+              radius: 30,
+              backgroundImage: AssetImage(product.imageUrl),
+            ),
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  product.title,
+                  style: TextStyle(fontSize: 18), // Increased font size
+                ),
+                SizedBox(height: 8), // Add padding between title and subtitle
+                Row(
+                  children: [
+                    IconButton(onPressed: () {
+                      quantityAdjust(product, quantity + 1);
+                    }, icon: Icon(Icons.add, size: 20.0)),
+                    SizedBox(
+                      width: 80,
+                        child: TextField(
+                        onSubmitted: (value) {
+                          final newQuantity = int.tryParse(value) ?? quantity;
+                          quantityAdjust(product, newQuantity);
+                        },
+                        controller: TextEditingController(text: quantity.toString()),
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(vertical: 0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                    IconButton(onPressed: () {
+                      quantityAdjust(product, quantity - 1);
+                    }, icon: Icon(Icons.remove, size: 20.0)),
+                  ],
+                ),
+              ],
             ),
           );
         },
+        separatorBuilder: (context, index) => Divider(
+          color: Colors.grey,
+          thickness: 1,
+        ),
       ),
     );
   }
